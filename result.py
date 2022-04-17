fb3ebd96d1be099344b26e55c62bbf8a7e539b03
Gregor von Laszewski <laszewski@gmail.com>
2022-04-17 02:16:41 -0400

    Create result.py

---
+++ result.py
@@ -0,0 +1,48 @@
+result.py:
+
+rivanna = {
+   "v_100_localscratch": {} ,
+   "a_100_project": {},
+}
+The number is the epoch
+rivanna["a_100_project"][2] = \ 
+"""
+put the csv lines for the result here
+"""
+rivanna["a_100_project"][10] = \ 
+"""
+put the csv lines for the result here
+"""
+rivanna["a_100_project"][20] = \ 
+"""
+put the csv lines for the result here
+"""
+rivanna["a_100_project"][30] = \ 
+"""
+put the csv lines for the result here
+"""
+rivanna["a_100_project"][40] = \ 
+"""
+put the csv lines for the result here
+"""
+rivanna["a_100_project"][50] = \ 
+"""
+put the csv lines for the result here
+"""
+rivanna["a_100_project"][60] = \ 
+"""
+put the csv lines for the result here
+"""
+rivanna["a_100_project"][66] = \ 
+"""
+put the csv lines for the result here
+"""
+rivanna["a_100_project"][70] = \ 
+"""
+put the csv lines for the result here
+"""
+# and so on till it fails
+
+# do this for localhost on bii_gpu partition also
+
+# do this for colab
